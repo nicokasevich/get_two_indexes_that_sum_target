51b93ceebb37144b9df0e0e43f313b9b5d55b76d
Nicolas Kasevich <nicolas_kasevich@epam.com>
2022-11-24 23:53:19 +0100

    Arme la plantilla del ejercicio

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hallar los dos indices que suman el objetivo (target)
+
+Asumir que siempre van a haber dos numeros que sumen el objetivo.
---
+++ tasks/tasks.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+def get_two_indexes_that_sum_target(numbers: List[int], target: int) -> List[int]:
+    """Returns the two indexes of the numbers array that sum target."""
+    pass
